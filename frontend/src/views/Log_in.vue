eb3bf6cc45b361fb3c3ec376007557765c7d5f3f
AlikBook <ivanarturograndi6@gmail.com>
2025-10-04 17:01:39 +0200

    Added Log in base form

--- frontend/src/views/Log_in.vue
+++ frontend/src/views/Log_in.vue
@@ -1,5 +1,11 @@
 <template>
+    <div>
+        <input type="text" placeholder="Enter your username">
+        <input type="password" placeholder="Enter your password">
+        <button>Log in</button>
+    </div>
     <h1>Log in</h1>
+    
 </template>
 
 <script setup lang="ts">
