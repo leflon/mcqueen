934fd329e3bf54d3b73dc378eedf859f41c476d6
Paul Leflon <leflon.paul@gmail.com>
2025-10-06 11:47:40 +0200

    feat(router): lazy load routes.

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,46 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router';
 
-import { createRouter, createWebHistory } from "vue-router";
-
-import Home from "./views/Home.vue";
-import CheckFlashcards from "./views/Check_flashcards.vue";
-import Edit_mode from "./views/Edit_mode.vue";
-import Practice from "./views/Practice.vue";
-import Creation_mode from "./views/Creation_mode.vue";
-import Auth from './views/Auth.vue';
+import Home from './views/Home.vue';
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home
-    },
-    {
-        path: "/flashcards",
-        name: "CheckFlashcards",
-        component: CheckFlashcards
-    },
-    {
-        path: "/edit_mode/:id",
-        name: "Edit_mode",
-        component: Edit_mode
-    },
-    {
-        path: "/practice/:id",
-        name: "Practice",
-        component: Practice
-    },
-    {
-        path: "/creation_mode",
-        name: "Creation_mode",
-        component: Creation_mode
-    },
-    {
-      path: '/auth',
-      name: 'Auth',
-      component: Auth
-    }
-
-
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/flashcards',
+    name: 'CheckFlashcards',
+    component: () => import('./views/Check_flashcards.vue')
+  },
+  {
+    path: '/edit_mode/:id',
+    name: 'Edit_mode',
+    component: () => import('./views/Edit_mode.vue')
+  },
+  {
+    path: '/practice/:id',
+    name: 'Practice',
+    component: () => import('./views/Practice.vue')
+  },
+  {
+    path: '/creation_mode',
+    name: 'Creation_mode',
+    component: () => import('./views/Creation_mode.vue')
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('./views/Auth.vue')
+  }
 ];
 
 export const router = createRouter({
